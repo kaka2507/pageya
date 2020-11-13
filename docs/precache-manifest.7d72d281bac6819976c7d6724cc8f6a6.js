2d3c0b56842eb8102cd283f113d893eeb50eb229
Bao Doan N <doanngocbao@gmail.com>
2020-11-13 14:11:46 +0700

    Upd: add demo

---
+++ docs/precache-manifest.7d72d281bac6819976c7d6724cc8f6a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2a8b1975654a48e6855a3f758aa37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a33689627b7d5aba696",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "7c053d5b727cbf644210",
+    "url": "/static/css/main.1956e2b3.chunk.css"
+  },
+  {
+    "revision": "7a33689627b7d5aba696",
+    "url": "/static/js/2.d020a4b1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d020a4b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c053d5b727cbf644210",
+    "url": "/static/js/main.39608925.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/static/js/main.39608925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49de1d56170d62cf3751",
+    "url": "/static/js/runtime-main.d0798475.js"
+  }
+]);
