ab47f1f63ff19f3c29690cec7079c3f88016bd33
Bao Doan N <doanngocbao@gmail.com>
2020-11-13 14:22:45 +0700

    Upd: adjust base url for demo

---
+++ packages/demo/build/precache-manifest.bb153fab123635135ddc9ac37bb00806.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698b6963d11c05c988f237724a89c7a8",
+    "url": "/pageya/index.html"
+  },
+  {
+    "revision": "aed5234eb39cfe1feed3",
+    "url": "/pageya/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "7c053d5b727cbf644210",
+    "url": "/pageya/static/css/main.1956e2b3.chunk.css"
+  },
+  {
+    "revision": "aed5234eb39cfe1feed3",
+    "url": "/pageya/static/js/2.c8e1ebdc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pageya/static/js/2.c8e1ebdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c053d5b727cbf644210",
+    "url": "/pageya/static/js/main.39608925.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/pageya/static/js/main.39608925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8263246b9293a43eb378",
+    "url": "/pageya/static/js/runtime-main.b720fe7b.js"
+  }
+]);
